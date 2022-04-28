af263de3c6bb55943b8b6c5128e9b4390b7949f4
bqcuong <bqcuong2212@gmail.com>
2022-04-28 14:10:13 +0200

    update plugins for Java

--- nexus/plugins/nexus/jmutrepair_vul4j.py
+++ nexus/plugins/nexus/jmutrepair_vul4j.py
@@ -21,19 +21,52 @@ class JMutRepairVul4JRepairTask(NexusHandler):
 
         # the project named is used to merge with universal working_dir defined here
         # e.g., /nexus/vul4j_a1bc/ ->/nexus/vul4j_a1bc/vul4j
+
+        # get default build configs from the project
+        failing_module = program_modules.get('failing_module')
+        src = program_modules.get('src_dir')
+        test = program_modules.get('test_dir')
+        src_class = program_modules.get('src_classes')
+        test_class = program_modules.get('test_classes')
+        jvm_version = program.build.get('version')
+
+        vul_build_args = vulnerability.build['args'].split()
+        for i in range(0, len(vul_build_args)):
+            if vul_build_args[i] == '--failing_module':
+                failing_module = vul_build_args[i+1]
+            if vul_build_args[i] == '--src_dir':
+                src = vul_build_args[i+1]
+            if vul_build_args[i] == '--test_dir':
+                test = vul_build_args[i+1]
+            if vul_build_args[i] == '--src_classes':
+                src_class = vul_build_args[i+1]
+            if vul_build_args[i] == '--test_classes':
+                test_class = vul_build_args[i+1]
+
+        if vulnerability.build['version']:
+            jvm_version = vulnerability.build['version']
+
+        if failing_module != 'root':
+            src = failing_module + '/' + src
+            test = failing_module + '/' + test
+            src_class = failing_module + '/' + src_class
+            test_class = failing_module + '/' + test_class
+
         repair_args = {
-            'src': program_modules.get('src_dir'),
-            'test': program_modules.get('test_dir'),
-            'src_class': program_modules.get('src_classes'),
-            'test_class': program_modules.get('test_classes'),
-            'jvm_version': program.build.get('version'),
+            'src': src,
+            'test': test,
+            'src_class': src_class,
+            'test_class': test_class,
+            'jvm_version': jvm_version,
             'classpath': cp_res,
             'project_name': program.name,
             'perfect_data': 'VUL4J/' + vulnerability.id
         }
 
-        # orbis_test_url = self.orbis.url(action='test', instance=context.benchmark.instance)
-        orbis_testbatch_url = "http://172.17.0.2:8080/testbatch"  # for only testing on my machine
+        print(repair_args)
+
+        orbis_testbatch_url = self.orbis.url(action='test', instance=context.benchmark.instance)
+        # orbis_testbatch_url = "http://172.17.0.2:8080/testbatch"  # for only testing on my machine
 
         test_all_cmd = Command(iid=program_instance.iid,
                                url=orbis_testbatch_url)
