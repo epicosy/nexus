514967a3dd6a3a4f29de00850fafa8e46d7fc871
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2022-05-03 17:36:36 +0100

    update to use new verison of apis

--- nexus/plugins/nexus/mutapr_cgc.py
+++ nexus/plugins/nexus/mutapr_cgc.py
@@ -11,71 +11,81 @@ def c_to_cpp(c_file: str):
     return re.sub(r'.c$', '.i', c_file)
 
 
-class MUTAPRCGCRepairTask(NexusHandler):
+class MUTAPRCGCTask(NexusHandler):
     class Meta:
-        label = 'mutapr_cgcrepair'
+        label = 'mutapr_cgc'
 
     def __init__(self, **kw):
-        super().__init__(tool='mutapr', benchmark='cgcrepair', **kw)
+        super().__init__(tool='mutapr', benchmark='cgc2', **kw)
 
     def run(self, program: Program, vulnerability: Vulnerability, context: Context):
-        manifest = program.get_manifest()
+        manifest = vulnerability.get_manifest()
         manifest.transform(c_to_cpp)
 
-        program_instance = self.orbis.checkout(context.benchmark.instance, program=program)
-        self.orbis.compile(context.benchmark.instance, program_instance=program_instance, args={'--save_temps': ''})
+        program_instance = self.orbis.checkout(context.benchmark.instance, vuln=vulnerability)
+        self.orbis.build(context.benchmark.instance, program_instance=program_instance, args={'save_temps': True})
+        manifest_path = vulnerability.get_manifest()
+        manifest_path.add_parent(program_instance.working_dir)
+        inst_files = manifest_path.to_str()
 
         # COVERAGE COMPILE
-        compile_command = Command(iid=program_instance.iid, args={'--cpp_files': '', '--exit_err': ''},
-                                  url=self.orbis.url(action='compile', instance=context.benchmark.instance),
-                                  placeholders={'--inst_files': '__INST_FILES__'})
+        compile_command = Command(iid=program_instance.iid, args={'cpp_files': True, 'exit_err': True,
+                                                                  'inst_files': inst_files},
+                                  url=self.orbis.url(action='build', instance=context.benchmark.instance),
+                                  placeholders={'fix_files': '__INST_FILES__'}, vid=vulnerability.id)
 
         # COVERAGE POS TESTS
-        pos_test_command = Command(iid=program_instance.iid, args={'--pos_tests': ''},
+        pos_test_command = Command(iid=program_instance.iid, args={'tests': list(program.oracle['cases'].keys())},
                                    url=self.orbis.url(action='test', instance=context.benchmark.instance),
-                                   placeholders={"--cov_out_dir": "__COV_OUT_DIR__",
-                                                 "--rename_suffix": "__RENAME_SUFFIX__"})
-
+                                   vid=vulnerability.id, placeholders={"cov_out_dir": "__COV_OUT_DIR__",
+                                                                       "rename_suffix": "__RENAME_SUFFIX__"})
+        pos_test_command.add_arg('cov_suffix', '.path')
+        pos_test_command.add_arg('cov_dir', str(program_instance.build_dir))
         # COVERAGE NEG TESTS
-        neg_test_command = Command(iid=program_instance.iid, args={'--neg_tests': ''},
+        neg_test_command = Command(iid=program_instance.iid, args={'tests': list(vulnerability.oracle['cases'].keys())},
                                    url=self.orbis.url(action='test', instance=context.benchmark.instance),
-                                   placeholders={"--cov_out_dir": "__COV_OUT_DIR__"})
+                                   vid=vulnerability.id, placeholders={"cov_out_dir": "__COV_OUT_DIR__"})
 
-        response = self.synapser.coverage(signals=[Signal(arg='compile', command=compile_command),
-                                                   Signal(arg='pos_tests', command=pos_test_command),
-                                                   Signal(arg='neg_tests', command=neg_test_command)],
+        neg_test_command.add_arg('cov_suffix', '.path')
+        neg_test_command.add_arg('cov_dir', str(program_instance.build_dir))
+
+        response = self.synapser.coverage(signals=[Signal(arg='--gcc', command=compile_command),
+                                                   Signal(arg='--good', command=pos_test_command),
+                                                   Signal(arg='--bad', command=neg_test_command)],
                                           program_instance=program_instance, manifest=manifest,
-                                          instance=context.tool.instance)
+                                          instance=context.tool.instance, iid=program_instance.iid)
         response_json = response.json()
         self.app.log.info("CID: " + str(response_json['cid']))
 
         if response.ok:
             # REPAIR COMPILE
-            pos_test_command = Command(iid=program_instance.iid, args={'--exit_fail': '', '--pos_tests': ''},
+            pos_test_command = Command(iid=program_instance.iid, args={'exit_fail': True, 'timeout': 10,
+                                                                       'tests': list(program.oracle['cases'].keys())},
                                        url=self.orbis.url(action='test', instance=context.benchmark.instance),
-                                       placeholders={"--out_file": "__EXTRA__"})
+                                       placeholders={"out_file": "out_file"}, vid=vulnerability.id)
 
             neg_test_command = Command(iid=program_instance.iid,
                                        url=self.orbis.url(action='test', instance=context.benchmark.instance),
-                                       args={'--exit_fail': '', '--neg_tests': '', '--neg_pov': ''},
-                                       placeholders={"--out_file": "__EXTRA__"})
+                                       args={'exit_fail': True, 'tests': list(vulnerability.oracle['cases'].keys()),
+                                             'neg_pov': True, 'timeout': 10},
+                                       placeholders={"out_file": "out_file"}, vid=vulnerability.id)
 
             compile_command = Command(iid=program_instance.iid,
-                                      url=self.orbis.url(action='compile', instance=context.benchmark.instance),
-                                      args={'--inst_files': manifest.format(delimiter=' '), '--cpp_files': '',
-                                            '--exit_err': ''},
-                                      placeholders={"--fix_files": "__EXTRA__"})
+                                      url=self.orbis.url(action='build', instance=context.benchmark.instance),
+                                      args={'inst_files': manifest_path.to_str(), 'cpp_files': True,
+                                            'exit_err': True},
+                                      placeholders={"fix_files": "out_file"}, vid=vulnerability.id)
 
             # REPAIR
             response = self.synapser.repair(signals=[Signal(arg='--gcc', command=compile_command),
                                                      Signal(arg='--good', command=pos_test_command),
                                                      Signal(arg='--bad', command=neg_test_command)],
                                             args={'--vn': 0},
-                                            program_instance=program_instance, manifest=manifest,
-                                            instance=context.tool.instance)
+                                            program_instance=program_instance, manifest=manifest.locs,
+                                            instance=context.tool.instance, iid=program_instance.iid)
             response_json = response.json()
             self.app.log.info("RID: " + str(response_json['rid']))
 
 
 def load(app):
-    app.handler.register(MUTAPRCGCRepairTask)
+    app.handler.register(MUTAPRCGCTask)
